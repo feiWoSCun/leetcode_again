9394d9d1fc5242b735575760f66077dc3e10547b
Hone too young <88073345+Hone-too-young@users.noreply.github.com>
2023-01-05 14:08:54 +0800

    2023.01.05

--- src/main/java/Honetooyoung/Y23M01D04/LICode3.java
+++ src/main/java/Honetooyoung/Y23M01D04/LICode3.java
@@ -1,6 +1,8 @@
 package Honetooyoung.Y23M01D04;
 
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.PriorityQueue;
 
 public class LICode3 {
     /*
@@ -26,8 +28,19 @@ class Solution {
         }
         return ans;
     }
-
+    public int lengthOfLongestSubstringPro(String s) {
+        PriorityQueue<String> queue=new PriorityQueue<>((s1, t1) -> t1.length()- s1.length());
+        int right=0,left=0;
+        for (; right < s.length(); right++) {
+            if(s.substring(left,right).contains(String.valueOf( s.charAt(right)))){
+                queue.offer(s.substring(left,right));
+                left=s.substring(left).indexOf(s.charAt(right))+1+left;
+            }
+        }
+        queue.offer(s.substring(left));
+        return queue.poll().length();
+    }
     public static void main(String[] args) {
-        System.out.println(new Solution().lengthOfLongestSubstring("abba"));
+        System.out.println(new Solution().lengthOfLongestSubstringPro("abbabdkbdhhhhsbsbsb"));
     }
 }
