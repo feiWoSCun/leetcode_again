35b0a71305a7a015d449973bf29ae4bec504e052
YxYL <yxyl6125@gmail.com>
2023-01-09 19:25:11 +0800

    [21] 合并两个有序链表

---
+++ src/main/java/YxYL/21.合并两个有序链表/21.合并两个有序链表.java
@@ -0,0 +1,35 @@
+import javax.lang.model.element.Element;
+import javax.security.auth.x500.X500Principal;
+import javax.swing.text.AbstractDocument.ElementEdit;
+
+/*
+ * @lc app=leetcode.cn id=21 lang=java
+ *
+ * [21] 合并两个有序链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1 == null || list2 == null) {
+            return list1 != null ? list1 : list2;
+        } else if (list1.val < list2.val) {
+            list1.next = mergeTwoLists(list1.next, list2);
+            return list1;
+        } else {
+            list2.next = mergeTwoLists(list1, list2.next);
+            return list2;
+        }
+    }
+}
+// @lc code=end
