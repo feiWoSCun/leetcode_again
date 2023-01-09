fac432af1ac04c914ee0ad3e0cc61b2c52cbaf5b
YxYL <yxyl6125@gmail.com>
2023-01-09 19:30:31 +0800

    [21] 合并两个有序链表

--- src/main/java/YxYL/21.合并两个有序链表/21.合并两个有序链表.java
+++ src/main/java/YxYL/21.合并两个有序链表/21.合并两个有序链表.java
@@ -18,6 +18,22 @@ import javax.swing.text.AbstractDocument.ElementEdit;
  * ListNode(int val) { this.val = val; }
  * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
+ * 
+ * 
+ * 将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+ * 
+ * 示例 1：
+ * 输入：l1 = [1,2,4], l2 = [1,3,4]
+ * 输出：[1,1,2,3,4,4]
+ * 
+ * 示例 2：
+ * 输入：l1 = [], l2 = []
+ * 输出：[]
+ * 
+ * 示例 3：
+ * 输入：l1 = [], l2 = [0]
+ * 输出：[0]
+ * 
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
