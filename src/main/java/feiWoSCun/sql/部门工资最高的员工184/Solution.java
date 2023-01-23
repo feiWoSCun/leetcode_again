c9b90910c34c7293c13df89b4e187efecb6b8067
“feiWoSCun” <2825097536@qq.com>
2023-01-24 01:33:15 +0800

    sql196

--- src/main/java/feiWoSCun/sql/部门工资最高的员工184/Solution.java
+++ src/main/java/feiWoSCun/sql/部门工资最高的员工184/Solution.java
@@ -79,4 +79,25 @@ Department 表:
 | IT         | Max      | 90000  |
 +------------+----------+--------+
 解释：Max 和 Jim 在 IT 部门的工资都是最高的，Henry 在销售部的工资最高。*/
+
+
+    /**
+     * SELECT
+     *     d.Name AS 'Department', e1.Name AS 'Employee', e1.Salary
+     * FROM
+     *     Employee e1
+     *         JOIN
+     *     Department d ON e1.DepartmentId = d.Id
+     * WHERE
+     *     3 > (SELECT
+     *             COUNT(DISTINCT e2.Salary)
+     *         FROM
+     *             Employee e2
+     *         WHERE
+     *             e2.Salary > e1.Salary
+     *                 AND e1.DepartmentId = e2.DepartmentId
+     */
+
+
+
 }
